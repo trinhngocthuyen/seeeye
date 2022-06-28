53f559e1dedd658326fbfdee3cbf1be78de261df
Thuyen Trinh <trinhngocthuyen@gmail.com>
2022-06-28 23:12:05 +0700

    add examples/ios (for integration tests)

---
+++ examples/ios/EX/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  EX
+//
+//  Created by Ngoc Thuyen Trinh on 7/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
---
+++ examples/ios/EXTests/EXTests.swift
@@ -0,0 +1,14 @@
+//
+//  EXTests.swift
+//  EXTests
+//
+//  Created by Ngoc Thuyen Trinh on 7/6/22.
+//
+
+import XCTest
+@testable import EX
+
+class EXTests: XCTestCase {
+  func testA1() throws { }
+  func testA2() throws { }
+}
