eadf073076a96d83ce1163e7b18c2c7046fab121
Thuyen Trinh <trinhngocthuyen@gmail.com>
2022-07-11 16:33:52 +0700

    CHORE: update sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,6 @@ project = 'seeeye'
 copyright = '2022, Thuyen Trinh'
 author = 'Thuyen Trinh'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
@@ -44,7 +43,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
