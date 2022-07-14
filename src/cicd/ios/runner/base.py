fa12fbcdf77217e23aed7cc1d32ad95de5cd2434
Thuyen Trinh <trinhngocthuyen@gmail.com>
2022-07-14 16:51:27 +0700

    FEAT: Handle job/action timeout

--- src/cicd/ios/runner/base.py
+++ src/cicd/ios/runner/base.py
@@ -1,7 +1,20 @@
+from cicd.core.utils.timeout import timeout
+
+
 class Runner:
     def run(self, **kwargs):
-        if self.action:
+        if not self.action:
+            return
+
+        timeout_in_sec = kwargs.get('timeout')
+        if not timeout_in_sec:
             return self.action.run(**kwargs)
+
+        @timeout(timeout_in_sec)
+        def run_with_timeout():
+            return self.action.run(**kwargs)
+
+        return run_with_timeout()
 
     @property
     def action(self):
