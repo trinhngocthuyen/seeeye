d7c4ee4778593a2be07f8be98ddc64013d915c7b
Thuyen Trinh <trinhngocthuyen@gmail.com>
2022-07-27 16:52:18 +0700

    FEAT: Separate build/test jobs for integration run/workflow/pipeline (#25)

--- scripts/integration_test_ios.sh
+++ scripts/integration_test_ios.sh
@@ -5,12 +5,31 @@ cd examples/ios
 mkdir -p tmp && printf 0 > tmp/test_retries_trace # To simulate test retries
 
 bundle install
+bundle exec pod install
 
-python3 -m cicd.ios.build \
-    --cocoapods \
-    --derived-data-path DerivedData \
-    --build-for-testing
-python3 -m cicd.ios.test \
-    --retries 1 \
-    --derived-data-path DerivedData \
-    --test-without-building
+function _exec_build() {
+    python3 -m cicd.ios.build \
+        --derived-data-path DerivedData \
+        --build-for-testing
+}
+
+function _exec_test() {
+    python3 -m cicd.ios.test \
+        --retries 1 \
+        --derived-data-path DerivedData \
+        --test-without-building
+}
+
+function _exec_coverage() {
+    echo "To be implemented"
+}
+
+if [[ -z ${ACTION} ]]; then
+    actions=(build test coverage)
+else
+    actions=(${ACTION})
+fi
+
+for action in ${actions[@]}; do
+    eval "_exec_${action}"
+done
