3e8abd14896db243b9a4a260a6b0512fd304c07d
Thuyen Trinh <trinhngocthuyen@gmail.com>
2022-08-07 14:01:14 +0700

    CHORE: Separate jobs for UI tests & unit tests

--- scripts/integration_test_ios.sh
+++ scripts/integration_test_ios.sh
@@ -18,17 +18,20 @@ function _exec_build() {
 }
 
 function _exec_test() {
+    local args=()
     if [[ ! -z ${ONLY_TESTING} ]]; then
-        only_testing_args=(
-            --only-testing ${ONLY_TESTING}
+        args+=(--only-testing ${ONLY_TESTING})
+    fi
+    if [[ ! -z ${NUM_OF_SHARDS} ]]; then
+        args+=(
+            --shards ${NUM_OF_SHARDS}
+            --shard-idx ${SHARD_IDX:-0}
         )
     fi
     python3 -m cicd.ios.cli test \
         ${common_args[@]} \
-        ${only_testing_args[@]} \
+        ${args[@]} \
         --retries 1 \
-        --shards 2 \
-        --shard-idx ${SHARD_IDX:-0} \
         --test-without-building
 }
 
