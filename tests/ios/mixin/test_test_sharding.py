fae1fb64174b8760228448b19faa0bcdcdecad40
Thuyen Trinh <trinhngocthuyen@gmail.com>
2023-08-21 09:13:39 +0700

    FEAT: CLI to extract tests (#66)

--- tests/ios/mixin/test_test_sharding.py
+++ tests/ios/mixin/test_test_sharding.py
@@ -14,8 +14,10 @@ def sut(bag):
         yield TestShardingMixin()
 
 
-def test_test_sharding(sut: TestShardingMixin, bag):
-    assert sut.extract_tests() == ['A1', 'A2', 'A3', 'B1', 'B2']
+def test_test_sharding(sut: TestShardingMixin, bag, tmp_path):
+    output_path = tmp_path / 'output.json'
+    assert sut.extract_tests(output_path=output_path) == ['A1', 'A2', 'A3', 'B1', 'B2']
     assert sut.get_test_shard(shards=2, shard_idx=1) == ['A1', 'A2', 'A3']
     assert sut.get_test_shard(shards=2, shard_idx=2) == ['B1', 'B2']
+    assert output_path.read_text() == '["A1", "A2", "A3", "B1", "B2"]'
     bag.action.run.assert_called()
